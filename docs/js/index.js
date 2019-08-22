5a01be0ff3cf510f8fd4b6ef0e12e9ac420f6f79
Addy Bhatia <addy@loftorbital.com>
2019-08-21 22:50:03 -0400

    improve animations on load, add Bio bg image

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
-  $(".figure").removeClass("temp");
+  
   $(".figure").addClass("animated");
+  $(".figure").addClass("flipInX");
+
   $(window).scroll(function(){                          
-    if ($(this).scrollTop() > 200) {
+    if ($(this).scrollTop() > 150) {
       $('.navbar').fadeIn(500);
     } else {
       $('.navbar').fadeOut(500);
     }
-    if ($(this).scrollTop() > 200) {
-      $('.row1').fadeIn(800);
+    if ($(this).scrollTop() > 150) {
+      $('.row1').fadeIn(500);
     } else {
       $('.row1').fadeOut(200);
     }
-    if ($(this).scrollTop() > 800) {
-      $('.row4').fadeIn(800);
+    if ($(this).scrollTop() > 750) {
+      $('.row2').fadeIn(500);
     } else {
-      $('.row4').fadeOut(200);
+      $('.row2').fadeOut(200);
+    }
+    if ($(this).scrollTop() > 1300) {
+      $('.row3').fadeIn(500);
+    } else {
+      $('.row3').fadeOut(200);
     }
   });
 });
