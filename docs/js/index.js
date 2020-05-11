7f07837a70eccf1bac00f77af172bb6686218658
Addy Bhatia <addy@loftorbital.com>
2020-05-11 17:27:42 -0400

    update modern ui

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,27 +2,36 @@ $(document).ready(function() {
   
   $(".figure").addClass("animated");
   $(".figure").addClass("flipInX");
+  $( "#software" ).toggle( "fold" );
 
-  $(window).scroll(function(){                          
-    if ($(this).scrollTop() > 150) {
-      $('.navbar').fadeIn(500);
-    } else {
-      $('.navbar').fadeOut(500);
-    }
-    if ($(this).scrollTop() > 150) {
-      $('.row1').fadeIn(500);
-    } else {
-      $('.row1').fadeOut(200);
-    }
-    if ($(this).scrollTop() > 750) {
-      $('.row2').fadeIn(500);
-    } else {
-      $('.row2').fadeOut(200);
-    }
-    if ($(this).scrollTop() > 1300) {
-      $('.row3').fadeIn(500);
-    } else {
-      $('.row3').fadeOut(200);
-    }
+
+  $("#soft_but").click(function() {
+    $( "#software" ).toggle( "fold" );
+    var el = $("#photo_but")[0]; 
+    if(el.style.opacity==0.5){el.style.opacity=1;}
+    else {el.style.opacity=0.5;}
   });
+
+  // $(window).scroll(function(){                          
+  //   if ($(this).scrollTop() > 150) {
+  //     $('.navbar').fadeIn(500);
+  //   } else {
+  //     $('.navbar').fadeOut(500);
+  //   }
+  //   if ($(this).scrollTop() > 150) {
+  //     $('.row1').fadeIn(500);
+  //   } else {
+  //     $('.row1').fadeOut(200);
+  //   }
+  //   if ($(this).scrollTop() > 750) {
+  //     $('.row2').fadeIn(500);
+  //   } else {
+  //     $('.row2').fadeOut(200);
+  //   }
+  //   if ($(this).scrollTop() > 1300) {
+  //     $('.row3').fadeIn(500);
+  //   } else {
+  //     $('.row3').fadeOut(200);
+  //   }
+  // });
 });
